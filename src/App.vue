1d7cfec55b5083209e1da33cd73e3cac6c248b1f
Daniil Zaburunov <zaburunov.2000@mail.ru>
2026-09-04 17:46:34 +0400

    1-basics: add styles for main

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,19 @@ import BaseButton from './components/BaseButton.vue';
 </script>
 
 <template>
-  <div>
+  <main class="main">
     <BaseButton>
       <template #icon>
-          +
+        +
       </template>
       Сохранить
     </BaseButton>
-  </div>
+  </main>
 </template>
+<style scoped>
+.main {
+  border-radius: 25px;
+  background: var(--color-bg-main);
+  padding: 60px 50px;
+}
+</style>
